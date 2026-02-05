16a04cf3b33b13a84e2bcbcee33c2b1622bbca3a
Maurice Lorenz <me@mauricelorenz.com>
2026-02-05 21:39:16 +0100

    Initial commit

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main() -> None:
+    """Run the main program."""
+    print("=== Command Quest ===")
+    if len(sys.argv) < 2:
+        print("No arguments provided!")
+        print(f"Program name: {sys.argv[0]}")
+    else:
+        i = 0
+        for arg in sys.argv:
+            if i == 0:
+                print(f"Program name: {sys.argv[i][2:]}")
+            else:
+                print(f"Argument {i}: {arg}")
+            i += 1
+    print(f"Total arguments: {len(sys.argv)}\n")
+
+
+if __name__ == "__main__":
+    main()
